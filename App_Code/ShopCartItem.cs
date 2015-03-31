86b8cdebb9c8dbf44662b74683c36fe7d64d5413
Wu Ke <wke918@aucklanduni.ac.nz>
2015-03-31 17:24:29 +1300

    Fixed some bugs, edited master page

---
+++ App_Code/ShopCartItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for ShopCartItem
+/// </summary>
+public class ShopCartItem
+{
+    public Book book { get; set; }
+    public int number { get; set; }
+    public int bookId { get { return book.id; } }
+    public ShopCartItem() { }
+    public ShopCartItem(Book theBook, int num)
+    {
+        book = theBook;
+        number = num;
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Book
+/// </summary>
+public class Book
+{
+    public int ID { get; set; }
+    public string Title { get; set; }
+    public double Price { get; set; }
+    public double Discount { get; set; }
+
+}
